f788150dad73a79ee90b9f942a96fd24f7c4e671
Evan <erflynn21@gmail.com>
2020-05-11 02:37:06 -0400

    Packets show up in terminal

--- replace_downloads.py
+++ replace_downloads.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python
 
+import netfilterqueue
+import scapy.all as scapy
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        print(scapy_packet.show())
+
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(1, process_packet)
+queue.run()
