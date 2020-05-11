3049a3732e123c090bba60472126b6796b49b794
Evan <erflynn21@gmail.com>
2020-05-11 02:42:44 -0400

    Program now filters http requests and responses

--- replace_downloads.py
+++ replace_downloads.py
@@ -7,7 +7,12 @@ import scapy.all as scapy
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
-        print(scapy_packet.show())
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("HTTP Request")
+            print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("HTTP Response")
+            print(scapy_packet.show())
 
     packet.accept()
 
